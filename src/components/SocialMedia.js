1a095142ed78ad13601703b612cc3a2c648539b8
Angel <gutierrezramosjoseangel@gmail.com>
2021-04-18 02:01:14 -0500

    PR changes on scss file

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -10,20 +10,20 @@ export const SocialMedia = () => {
                 <Col sm={12} md={5} className='socialMediaContainer d-flex flex-column justify-content-center align-items-center mb-5'>
                     <h4 className='text-white pb-4'>Visita nuestra página oficial</h4>
                     <a href="https://bedu.org/" target="_blank">
-                        <img src={BeduLogo} alt="Bedu Logo Small" style={{height: '60px', width: '50px'}}/>
+                        <img className='beduLogo' src={BeduLogo} alt="Bedu Logo Small"/>
                     </a>
                 </Col>
                 <Col sm={12} md={5} className='socialMediaContainer d-flex flex-column justify-content-center align-items-center mb-5 offset-md-1'>
                     <h4 className='text-white pb-4'>Visita nuestra redes</h4>
                     <div className='d-flex justify-content-around w-100 pr-5 pl-5'>
                         <a href='https://www.facebook.com/BeduOrg' target="_blank">
-                            <i class="bi bi-facebook text-white" style={{fontSize: '2.5rem'}}></i>
+                            <i class="bi bi-facebook socialMediaLogo text-white"></i>
                         </a>
                         <a href="https://www.instagram.com/bedu_org/" target="_blank">
-                            <i class="bi bi-instagram text-white" style={{fontSize: '2.5rem'}}></i>
+                            <i class="bi bi-instagram socialMediaLogo text-white"></i>
                         </a>
                         <a href="https://www.linkedin.com/school/beduorg/mycompany/" target="_blank">
-                            <i class="bi bi-linkedin text-white" style={{fontSize: '2.5rem'}}></i>
+                            <i class="bi bi-linkedin socialMediaLogo text-white"></i>
                         </a>
                     </div>
                 </Col>
