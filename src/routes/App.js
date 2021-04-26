d57f2801d13bd5ad68ac2fa54cd6374b6e908234
Angel <gutierrezramosjoseangel@gmail.com>
2021-04-26 15:36:43 -0500

    Login Functionality added with redux and product CRUD in progress

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Layout } from '../hoc/Layout/Layout.js';
 import { Home } from '../views/Home/Home';
 import { Login } from '../views/Login/Login';
+import { ProductEditScreen } from '../views/Products/ProductEditScreen.js';
+import { Products } from '../views/Products/Products.js';
 import { Register } from '../views/Register/Register';
 
 export const App = () => {
@@ -13,6 +15,8 @@ export const App = () => {
                     <Route exact path='/' component={Home} />
                     <Route exact path='/login' component={Login} />
                     <Route exact path='/register' component={Register} />
+                    <Route exact path='/admin/productlist' component={Products} />
+                    <Route exact path='/admin/product/:id/edit' component={ProductEditScreen} />
                 </Switch>
             </Layout>
         </BrowserRouter>
---
+++ src/views/Products/Products.js
@@ -0,0 +1,96 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Row, Col, Button, Table, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { BsTrashFill } from 'react-icons/bs';
+import { FaEdit } from 'react-icons/fa';
+import { listProducts, deleteProduct } from '../../actions/productActions';
+import { Message } from '../../components/Alert/Alert';
+import { Loader } from '../../components/Loader/Loader';
+
+export const Products = () => {
+    const dispatch = useDispatch();
+
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products, page, pages } = productList;
+
+    const productDelete = useSelector(state => state.productDelete)
+    const { 
+        loading: loadingDelete, 
+        error: errorDelete, 
+        success: successDelete 
+    } = productDelete
+
+    useEffect(() => {
+        dispatch(listProducts('', 1));
+    }, [dispatch, successDelete]);
+
+
+    const createProductHandler = () => {
+        console.log("Product Created");
+    }
+
+    const deleteProductHandler = (id) => {
+        if (window.confirm('¿Deseas eliminar el producto?')) {
+            dispatch(deleteProduct(id))
+        }
+    }
+
+    return (
+        <Container>
+            <Row className="align-items-center">
+                <Col>
+                    <h1>Productos</h1>
+                </Col>
+                <Col className='text-right'>
+                    <Button className='my-3' onClick={createProductHandler}>
+                        <i className='fas fa-plus'></i> Create Product
+                    </Button>
+                </Col>
+            </Row>
+            {loadingDelete && <Loader />}
+            {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+            {loading ? <Loader /> : 
+                (
+                    <>
+                        <Table striped bordered hover responsive className='table-sm'>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Nombre</th>
+                                    <th>Precio</th>
+                                    <th>Categoria</th>
+                                    <th>Marca</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    products.map(product => (
+                                        <tr key={product._id}>
+                                            <td>{product._id}</td>
+                                            <td>{product.name}</td>
+                                            <td>${product.price}</td>
+                                            <td>{product.category}</td>
+                                            <td>{product.brand}</td>
+                                            <td>
+                                                <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                                                    <Button variant='light' className='btn-sm'>
+                                                        <FaEdit />
+                                                    </Button>
+                                                </LinkContainer>
+                                                <Button variant='danger' className='btn-sm' onClick={() => deleteProductHandler(product._id)}>
+                                                    <BsTrashFill />
+                                                </Button>
+                                            </td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </Table>
+                    </>
+                )
+            }
+        </Container>
+    )
+}
