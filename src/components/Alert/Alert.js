f769055b54cc108eecd031cc05c582cc6ec75617
Angel <gutierrezramosjoseangel@gmail.com>
2021-05-01 01:55:40 -0500

    Add new prop to manage text position on alert.

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Alert } from 'react-bootstrap';
 
-export const Message = ({variant, children}) => {
+export const Message = ({variant, textPosition, children}) => {
     return (
-        <Alert className="text-center" variant={variant}>
+        <Alert className={`text-${textPosition}`} variant={variant}>
             {children}
         </Alert>
     )
 };
 
 Message.defaultProps = {
+    textPosition: 'center',
     variant: 'info'
 }
