8485f9f20e8f30280dbdb6455698d7079363cfc2
Angel <gutierrezramosjoseangel@gmail.com>
2021-05-03 01:00:39 -0500

    Redux flow added to handle update to deliver and pay order

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -11,7 +11,13 @@ import {
     LIST_MY_ORDERS_FAIL,
     ORDER_DETAILS_REQUEST,
     ORDER_DETAILS_SUCCESS,
-    ORDER_DETAILS_FAIL
+    ORDER_DETAILS_FAIL,
+    ORDER_PAY_REQUEST,
+    ORDER_PAY_SUCCESS,
+    ORDER_PAY_FAIL,
+    ORDER_DELIVER_REQUEST,
+    ORDER_DELIVER_SUCCESS,
+    ORDER_DELIVER_FAIL
 } from '../constants/orderConstants';
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -136,5 +142,68 @@ export const listMyOrders = () => async (dispatch, getState) => {
                 ? error.response.data.message
                 : error.message
         });
+    }
+}
+
+export const payOrder = (orderId, paymentResult) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: ORDER_PAY_REQUEST
+        });
+
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await beduStoreAPI.put(`/orders/${orderId}/pay`, paymentResult, config);
+
+        dispatch({
+            type: ORDER_PAY_SUCCESS,
+            payload: data
+        });
+
+    } catch (error) {
+        dispatch({
+            type: ORDER_PAY_FAIL,
+            payload: error.response && error.response.data.error
+                ? error.response.data.error
+                : error.message
+        })
+    }
+}
+
+export const deliverOrder = (order) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: ORDER_DELIVER_REQUEST
+        });
+
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data: { data } } = await beduStoreAPI.put(`/orders/${order._id}/deliver`,{},config);
+
+        dispatch({
+            type: ORDER_DELIVER_SUCCESS,
+            payload: data
+        });
+
+    } catch (error) {
+        dispatch({
+            type: ORDER_DELIVER_FAIL,
+            payload: error.response && error.response.data.error
+                ? error.response.data.error
+                : error.message
+        })
     }
 }
--- src/reducers/orderReducers.js
+++ src/reducers/orderReducers.js
@@ -12,6 +12,14 @@ import {
     ORDER_DETAILS_REQUEST,
     ORDER_DETAILS_SUCCESS,
     ORDER_DETAILS_FAIL,
+    ORDER_PAY_REQUEST,
+    ORDER_PAY_SUCCESS,
+    ORDER_PAY_FAIL,
+    ORDER_PAY_RESET,
+    ORDER_DELIVER_REQUEST,
+    ORDER_DELIVER_SUCCESS,
+    ORDER_DELIVER_FAIL,
+    ORDER_DELIVER_RESET
 } from '../constants/orderConstants';
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -111,6 +119,62 @@ export const listMyOrdersReducer = (state = { orders:[] }, action) => {
         case LIST_MY_ORDERS_RESET:
             return { orders: [] }
 
+        default:
+            return state;
+    }
+} 
+
+export const orderPayReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ORDER_PAY_REQUEST:
+            return {
+                loading: true,
+                success: false
+            }
+        
+        case ORDER_PAY_SUCCESS:
+            return {
+                loading: false,
+                success: true
+            }
+
+        case ORDER_PAY_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+
+        case ORDER_PAY_RESET:
+            return {}
+    
+        default:
+            return state;
+    }
+} 
+
+export const orderDeliverReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ORDER_DELIVER_REQUEST:
+            return {
+                loading: true,
+                success: false
+            }
+        
+        case ORDER_DELIVER_SUCCESS:
+            return {
+                loading: false,
+                success: true
+            }
+
+        case ORDER_DELIVER_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+
+        case ORDER_DELIVER_RESET:
+            return {}
+    
         default:
             return state;
     }
