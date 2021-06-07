6ae4d6f7b436f6a71da6a51bdde127f835ad57af
Angel <gutierrezramosjoseangel@gmail.com>
2021-06-06 23:47:44 -0500

    Search Results View created and pagination modified

--- src/components/Pagination/PaginationComponent.js
+++ src/components/Pagination/PaginationComponent.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Pagination } from 'react-bootstrap';
+import { useHistory } from 'react-router';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import './PaginationComponent.scss';
 
-export const PaginationComponent = ({ page, totalPages, center }) => {
+export const PaginationComponent = ({ page, totalPages, center, keyword = '' }) => {
+    const { location: { pathname } } = useHistory();
+
     return totalPages > 1 && (
         <Pagination size='md' className={center && 'justify-content-center'}>
             {[...Array(totalPages).keys()].map(x => (
                 <LinkContainer
                     key={x + 1}
-                    to={`/store/${x + 1}`}
+                    to={(pathname.includes('store')) ? `/store/${x + 1}` : `/search/${keyword}/page/${x + 1}`}
                 >
                     <Pagination.Item active={x + 1 === page} style={{backgroundColor: 'orange'}}>
                         {x + 1}
