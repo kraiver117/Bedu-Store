6d524a7a50e0b19135d38eae75bab10e8534ae7b
Alexis Garcia <alexgarciamo14@gmail.com>
2021-06-20 19:15:08 -0500

    Navbar was redesigned

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Nav, Navbar, NavDropdown, Badge } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, Badge, Container, FormControl, Button, InputGroup } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { FaShoppingCart } from 'react-icons/fa';
 import { 
@@ -10,15 +10,19 @@ import {
     BsPeopleFill, 
     BsBoxArrowRight, 
     BsReverseLayoutTextSidebarReverse, 
-    BsInboxFill 
+    BsInboxFill,
+    BsSearch
 } from 'react-icons/bs';
 import { logout } from '../../actions/userActions';
 import { Link } from 'react-router-dom';
 import './Header.scss';
 import { SearchInput } from '../SearchInput/SearchInput';
 import { searchProducts } from '../../actions/productActions';
+import { useLocation } from 'react-router-dom';
 
 export const Header = () => {
+    const location = useLocation();
+    
     const dispatch = useDispatch();
     const [searchValue, setSearchValue] = useState('');
 
@@ -37,31 +41,43 @@ export const Header = () => {
     }
 
     return (
-        <Navbar className='navbar-fixed p-navbar' bg="light" expand="lg">
+        <div className="navbar-fixed">
+        <Navbar className='p-navbar' bg="white" expand="lg">
             <LinkContainer to='/'>
                 <Navbar.Brand className="navbar-title">Bedu Store</Navbar.Brand>
             </LinkContainer>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="m-auto">
-                    <LinkContainer to='/store'>
-                        <Nav.Link className="navbar-links">
-                            Productos
-                        </Nav.Link>
-                    </LinkContainer>
-                </Nav>
-                <div className="navbar-right d-flex justify-content-around align-items-center">
+                <Nav className="w-80 py-2 my-md-0">
                     <SearchInput 
                         value={ searchValue } 
                         onChange={ setSearchValue } 
                         onSubmit={ searchProduct } 
                     />
+                </Nav>
+                <div className="d-flex justify-content-around">
                     <Link className='text-dark mx-4' to='/cart'>
                         {
                             cartItems.length > 0 && <Badge className='bg-color-orange text-white' pill>{cartItems.length}</Badge>
                         }
                         <FaShoppingCart size={20}/>    
                     </Link>
+                    { 
+                     !userInfo ?
+                        <div>
+                        <Link to='/login' className="pr-2 border-right text-dark font-weight-bolder">
+                            Iniciar sesión
+                        </Link>
+                        <Link to='/register' className="ml-2 text-dark font-weight-bolder">
+                            Registrarse
+                        </Link>
+                        </div>
+                        : ''
+                    }
+                </div>
+                {
+                    userInfo ? 
+                    <div className="navbar-right d-flex justify-content-around align-items-center">
                     <NavDropdown 
                         id="user-dropdown" 
                         title={userInfo 
@@ -138,7 +154,34 @@ export const Header = () => {
                         }
                     </NavDropdown>
                 </div>
+                    : ''
+                }
             </Navbar.Collapse>
         </Navbar>
+        <div className="bg-light px-0">
+            <div className="row row-cols-4 py-2 mx-auto products-category">
+                <div className="text-center font-weight-bolder">
+                    <Link to='/store?category=Todas' className="text-dark">
+                        Todas   
+                    </Link>
+                </div>
+                <div className="text-center font-weight-bolder">
+                    <Link to='/store?category=Playera' className="text-dark">
+                        Playera
+                    </Link>
+                </div>
+                <div className="text-center font-weight-bolder">
+                    <Link to='/store?category=Mochila' className="text-dark">
+                        Mochila
+                    </Link>
+                </div>
+                <div className="text-center font-weight-bolder">
+                    <Link to='/store?category=Taza' className="text-dark">
+                        Taza
+                    </Link>
+                </div>
+            </div>
+        </div>
+      </div>
     );
 };
--- src/views/Store/ProductsStore.js
+++ src/views/Store/ProductsStore.js
@@ -1,38 +1,41 @@
 import React, { useEffect } from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import {  listProductsWithQuery } from '../../actions/productActions';
 import { Message } from '../../components/Alert/Alert';
 import { Loader } from '../../components/Loader/Loader';
 import { PaginationComponent as Pagination } from '../../components/Pagination/PaginationComponent';
 import { ProductCard } from '../../components/ProductCard/ProductCard';
+import qs from "qs";
+// const categories = [
+//     "Todas",
+//     "Playera",
+//     "Mochila",
+//     "Taza"
+// ]
 
-const categories = [
-    "Todas",
-    "Playera",
-    "Mochila",
-    "Taza"
-]
-
-export const ProductsStore = ({ match }) => {
+export const ProductsStore = ({ match, location }) => {
     const dispatch = useDispatch();
     let pageNumber = match.params.page || 1;
+    const { category = '' } = qs.parse(location.search, {
+        ignoreQueryPrefix: true
+    });
     
     const productListState = useSelector(state => state.productsListWithQuery);
     const { products = [], loading, error, totalPages } = productListState;
 
     useEffect(() => {
-        dispatch(listProductsWithQuery('', pageNumber, 3));
-    }, [dispatch, pageNumber]);
-
-    const handleFilterCategory = (category) => {
-        pageNumber = 1;
         dispatch(listProductsWithQuery(category, pageNumber, 3));
-    }
+    }, [dispatch, pageNumber, category]);
+
+    // const handleFilterCategory = (category) => {
+    //     pageNumber = 1;
+    //     dispatch(listProductsWithQuery(category, pageNumber, 3));
+    // }
 
     return (
         <Container className='my-4'>
-            <Row className='justify-content-center'>
+            {/* <Row className='justify-content-center'>
                 {
                     categories.map((category, index) => (
                         <Button
@@ -45,7 +48,7 @@ export const ProductsStore = ({ match }) => {
                         </Button>
                     ))
                 }
-            </Row>
+            </Row> */}
             <Row className='justify-content-center my-4'>
                 { loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : 
                     products.map((product, index) => (
