8c3e79f99f190adb5a9431810286de4261bca453
Angel <gutierrezramosjoseangel@gmail.com>
2021-06-24 23:27:44 -0500

    Enhancement on main carousel and new images made.

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { CardsCarousel } from '../../components/CardsCarousel/CardsCarousel';
 import { MainCarousel } from '../../components/MainCarousel/MainCarousel';
 import { SocialMedia } from '../../components/SocialMedia/SocialMedia';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
 
 export const Home = () => {
+
+    const { width } = useWindowDimensions();
+    console.log(width)
+
     return (
         <>
-            <MainCarousel />
+            { width >= 800 && <MainCarousel /> }
             <CardsCarousel title='Productos más comprados' />
             <SocialMedia />
             <CardsCarousel title='Nuestros Productos' />
